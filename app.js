5300245569f79fe7cc8c21cd1471d03f8c5d7172
dragon9481 <77512206+dragon9481@users.noreply.github.com>
2021-02-14 10:40:07 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+// Variables
+let contador = 0
+const cero = true
+
+// Funciones
+function countingClicks() {
+    contador = contador+1
+    document.getElementById("counting").innerHTML = contador;
+}
